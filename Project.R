cba4e2b672d40b92e04414f76c7fd58e544dd97e
Andriy <zubr.andr@gmail.com>
2015-04-26 14:06:54 +0200

    First commit of dataset.

---
+++ Project.R
@@ -0,0 +1,4 @@
+# Download and unzip dataset
+download.file(url = 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip', destfile = 'dataset.zip')
+unzip(zipfile = 'dataset.zip')
+
